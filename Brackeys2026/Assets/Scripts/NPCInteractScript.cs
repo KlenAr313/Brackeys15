e72ba0cb2e89a6cc6554b43e84e7d5ef94940c49
QuiteSurreal <82947991+QuiteSurreal@users.noreply.github.com>
2026-02-16 14:06:06 +0100

    NPC interaction script

---
+++ Brackeys2026/Assets/Scripts/NPCInteractScript.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using System.ComponentModel;
+using UnityEngine.InputSystem;
+using System.Linq.Expressions;
+
+public class NPCInteractScript : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI NPCDialogue;
+    [SerializeField] private Canvas dialogueCanvas;
+    [SerializeField] private float dialogueSpeed;
+    [SerializeField] private string[] sentences;
+    [SerializeField] private float interactionRange = 2f;
+    [SerializeField] private KeyCode interactionKey = KeyCode.E;
+    
+    private bool playerInRange = false;
+    private bool dialogueActive = false;
+    private int ind = 0;
+    private Transform player;
+    private bool doneWriting = true;
+
+    void Start()
+    {
+        // MAKE PLAYER BEFORE
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        if (dialogueCanvas != null)
+            dialogueCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        playerInRange = distanceToPlayer <= interactionRange;
+
+        bool pressed = false;
+
+        if (Keyboard.current != null)
+        {
+            Key parsedKey;
+            if (System.Enum.TryParse<Key>(interactionKey.ToString(), out parsedKey))
+            {
+                var keyControl = Keyboard.current[parsedKey];
+                if (keyControl != null && keyControl.wasPressedThisFrame)
+                    pressed = true;
+            }
+        }
+
+
+        if (playerInRange && pressed)
+        {
+            ShowDialogue();
+            NextSentence();
+        }
+        else if (!playerInRange && dialogueActive == true)
+        {
+            HideDialogue();
+        }
+    }
+
+    private void ShowDialogue()
+    {
+        dialogueActive = true;
+        if (dialogueCanvas != null)
+        {
+            dialogueCanvas.enabled = true;
+            
+        }
+    }
+
+    private void HideDialogue()
+    {
+        dialogueActive = false;
+        if (dialogueCanvas != null)
+            dialogueCanvas.enabled = false;
+        
+        if (NPCDialogue != null)
+            NPCDialogue.text = "";
+    }
+
+    void NextSentence()
+    {
+        if (ind <= sentences.Length - 1 && doneWriting)
+        {
+            NPCDialogue.text = "";
+            StartCoroutine(WriteSentence());
+        }
+        else
+        {
+            ind = 0;
+            HideDialogue();
+        }
+    }
+
+    IEnumerator WriteSentence()
+    {
+        doneWriting = false;
+        foreach(char character in sentences[ind].ToCharArray())
+        {
+            NPCDialogue.text += character;
+            yield return new WaitForSeconds(dialogueSpeed);
+        }
+        doneWriting = true;
+        ind++;
+    }
+}
