08281304e3a3d6be529c6b7a2e835d0b7492e15e
Viktor Ádám Trefiman <varazsgoreny765@gmail.com>
2026-02-16 02:12:15 +0100

    Adding basic movement files to the project and setting up a basic scene with a controllable cube.

---
+++ Brackeys2026/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerScript : EntityControllerScript
+{
+	[SerializeField] private InputAction playerMove;
+	[SerializeField] private InputAction playerJump;
+
+	private void OnEnable()
+	{
+		playerMove.Enable();
+		playerJump.Enable();
+	}
+
+	private void OnDisable()
+	{
+		playerMove.Disable();
+		playerJump.Disable();
+	}
+
+	void Update()
+	{
+		direction = Vector2Rotate(playerMove.ReadValue<Vector2>(), tr.localEulerAngles.y);
+	}
+}
