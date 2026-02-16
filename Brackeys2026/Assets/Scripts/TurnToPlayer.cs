ac96af93d244a1652a347c008b48d2bf406db966
Klenovszky Áron <aron.klenovszky@gmail.com>
2026-02-16 21:43:53 +0100

    Script prefab and others.

---
+++ Brackeys2026/Assets/Scripts/TurnToPlayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TurnToPlayer : MonoBehaviour
+{
+    public Transform player; // Drag the camera here in the editor
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        direction.y = 0; 
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
