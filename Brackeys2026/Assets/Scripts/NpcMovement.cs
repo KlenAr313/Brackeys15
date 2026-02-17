7c2467d92356f56b40f8eda882205ee4a5b83df9
Klenovszky Áron <aron.klenovszky@gmail.com>
2026-02-17 21:42:24 +0100

    Wall detection

--- Brackeys2026/Assets/Scripts/NpcMovement.cs
+++ Brackeys2026/Assets/Scripts/NpcMovement.cs
@@ -17,6 +17,8 @@ public class NpcMovement : MonoBehaviour
     private Vector3 playerDirection;
     private Vector3 idleDirection;
     private float idleTimer;
+    private float wallTimer;
+    private Vector3 wallDirection;
 
     private float originalY;
 
@@ -28,13 +30,21 @@ public class NpcMovement : MonoBehaviour
 
     void Update()
     {
-        playerDirection = new Vector3(player.position.x - transform.position.x, 0, player.position.z - transform.position.z);
+        playerDirection = new Vector3(
+            player.position.x - transform.position.x, 0, 
+            player.position.z - transform.position.z
+        );
+
         if(playerDirection.magnitude < detectionRange && playerDirection.magnitude > stopDistance)
-            transform.position += playerDirection.normalized * speed * Time.deltaTime;
+            transform.position += (playerDirection.normalized + wallDirection).normalized * speed * Time.deltaTime;
         else if (playerDirection.magnitude > detectionRange)
             Idle();
 
         transform.position = new Vector3(transform.position.x, originalY, transform.position.z);
+        
+        wallTimer -= Time.deltaTime;
+        if (wallTimer <= 0)
+            wallDirection = Vector3.zero;
     }
 
     private void Idle()
@@ -44,13 +54,16 @@ public class NpcMovement : MonoBehaviour
             if(UnityEngine.Random.value < 0.5f)
                 idleDirection = Vector3.zero;
             else
-                idleDirection = new Vector3(UnityEngine.Random.Range(-1f, 1f), 0f, UnityEngine.Random.Range(-1f, 1f)).normalized;
+                idleDirection = new Vector3(
+                    UnityEngine.Random.Range(-1f, 1f), 0f, 
+                    UnityEngine.Random.Range(-1f, 1f)
+                ).normalized;
 
             idleTimer = UnityEngine.Random.Range(0.2f, 1f);
         }
         else
         {
-            transform.position += idleDirection * idleSpeed * Time.deltaTime;
+            transform.position += (idleDirection + wallDirection).normalized * idleSpeed * Time.deltaTime;
             idleTimer -= Time.deltaTime;
         }
     }
@@ -62,6 +75,13 @@ public class NpcMovement : MonoBehaviour
             idleDirection = new Vector3(collision.contacts[0].normal.x, 0, collision.contacts[0].normal.z).normalized;
             idleTimer = UnityEngine.Random.Range(0.2f, 1f);
             Debug.Log("Collided with enemy, changing direction to: " + idleDirection);
+        }
+        else if (collision.gameObject.CompareTag("Wall"))
+        {
+            wallDirection = new Vector3(collision.contacts[0].normal.x, 0, collision.contacts[0].normal.z).normalized;
+            wallTimer = 0.4f;
+            idleDirection = Vector3.zero; 
+            Debug.Log("Collided with wall, changing direction to: " + wallDirection);
         }
     }
 
