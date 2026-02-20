84587177ac6b93c1eac179294fc8adda225c45bc
donovics <kerchnerdani@gamil.com>
2026-02-20 12:15:55 +0100

    Sewer map 1.6

--- Brackeys2026/Assets/Scripts/MovementScript.cs
+++ Brackeys2026/Assets/Scripts/MovementScript.cs
@@ -43,7 +43,7 @@ public class MovementScript : MonoBehaviour
 		force = controller.MovementSpeed * Time.fixedDeltaTime * controller.Direction;
 		if (inWater)
 		{
-			force = force * 0.4f;
+			force *= speedMody;
 		}
 		rigidbody.linearVelocity = new Vector3(force.x, rigidbody.linearVelocity.y, force.y);
 
