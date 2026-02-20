efb7ae2de2d37c51143ae2fd9268a7cead24f0f0
Klenovszky Áron <aron.klenovszky@gmail.com>
2026-02-20 10:53:53 +0100

    Merge NpcMovement to EnemmyBase

--- Brackeys2026/Assets/Scripts/EnemyBase.cs
+++ Brackeys2026/Assets/Scripts/EnemyBase.cs
@@ -7,11 +7,25 @@ public class EnemyBase : MonoBehaviour
     [SerializeField] private int damage = 5;
     [SerializeField] private float damageInterval = 3;
 
+    [SerializeField] private Transform player;
+    [SerializeField] private UnityEngine.AI.NavMeshAgent agent;
+    [SerializeField] private float detectionRange = 10f;
+
+    private float timer = 0;
+    [SerializeField] private float UpdateInterval = 0.5f;
+    [SerializeField] private bool triggered = true;
+    [SerializeField] private bool stayTriggered = true;
+
+    private SlotManager slotManager;
+
+    public GameObject currentSlot;
+
     private bool inCombat = false;
 
     private float damageTimer = 0;
 
     public bool InCombat { get => inCombat; set => inCombat = value; }
+    public void Trigger() { triggered = true; }
 
     public void TakeDamage(int amount)
     {
@@ -28,8 +42,16 @@ public class EnemyBase : MonoBehaviour
         Destroy(gameObject);
     }
 
+    void Start()
+    {
+        slotManager = FindFirstObjectByType<SlotManager>();
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
     void Update()
     {
+        Movement();
         if(damageTimer >= damageInterval)
         {
             damageTimer = 0;
@@ -40,5 +62,55 @@ public class EnemyBase : MonoBehaviour
         }
 
         damageTimer += Time.deltaTime;
+    }
+
+    void Movement()
+    {
+        if(timer < UpdateInterval)
+        {
+            timer += Time.deltaTime;
+            return;
+        }
+
+        timer = 0;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer < detectionRange && currentSlot == null && triggered && seesPlayer())
+        {
+
+            currentSlot = slotManager.GetAvailabeClosestSlot(transform.position);
+            if (currentSlot != null)
+            {
+                agent.SetDestination(currentSlot.transform.position);
+                inCombat = true;
+            }
+        }
+        else if (distanceToPlayer >= detectionRange && triggered)
+        {
+            agent.SetDestination(transform.position);
+            slotManager.ResetSlot(currentSlot);
+            currentSlot = null;
+            if(!stayTriggered)
+                triggered = false;
+            inCombat = false;
+        }
+
+        if (triggered && currentSlot != null)
+        {
+            agent.SetDestination(currentSlot.transform.position);
+            inCombat = true;
+        }
+    }
+
+    bool seesPlayer()
+    {
+        Vector3 directionToPlayer = player.position - transform.position;
+        Ray ray = new Ray(transform.position, directionToPlayer);
+
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, detectionRange, LayerMask.GetMask("Default"), QueryTriggerInteraction.Ignore))
+            return false;
+
+        return true;
     }
 }
