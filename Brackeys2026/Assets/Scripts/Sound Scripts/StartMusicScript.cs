e5455c9fd394a4ec08ca4684cb11b4918262c468
QuiteSurreal <82947991+QuiteSurreal@users.noreply.github.com>
2026-02-22 08:01:37 +0100

    wow

--- Brackeys2026/Assets/Scripts/Sound Scripts/StartMusicScript.cs
+++ Brackeys2026/Assets/Scripts/Sound Scripts/StartMusicScript.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class StartMusicScript : MonoBehaviour
 {
+    private bool triggered = false;
     private void OnTriggerEnter(Collider collision)
 	{
-        if (collision.gameObject.tag.ToLower() == "player")
+        if (collision.gameObject.tag.ToLower() == "player" && !triggered)
 		{
+            triggered = true;
 			TriggerMusic();
 		}
 	}
